189a45b55d745bc1852ba0b96f2ebf1dffd0df33
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-12-21 13:36:11 -0500

    retrieving data from API

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
-import Search from "./Components/Search"
+import Search from "./Components/Search";
 
 class App extends Component {
+  state = {
+    searchBookTitle: "",
+    books: []
+  };
+
+  handleTitleChange = e => {
+    this.setState({ searchBookTitle: e.target.value });
+  };
+
+  handleSearchBooks = e => {
+    e.preventDefault()
+    const title = this.state.searchBookTitle
+    fetch(
+      "https://www.googleapis.com/books/v1/volumes?q=" + title + "&key=AIzaSyDwSps7qU62IJji7xSY5EkpSAcyApIcyqA"
+    )
+      .then(res => res.json())
+      .then(data =>
+        data.items.map(book => {
+          return console.log(book);
+        })
+      );
+  };
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Google Book Search</h2>
         </div>
-        <Search />
+        <Search
+          handleTitleChange={this.handleTitleChange}
+          searchBooks={this.handleSearchBooks}
+        />
       </div>
     );
   }
--- create-react-express/client/src/Components/Search.js
+++ create-react-express/client/src/Components/Search.js
@@ -8,8 +8,8 @@ class Search extends Component {
         <h2 className="alert alert-success">Search Books</h2>
         <form>
             <div className="form-group">
-                <input type="text" placeholder="Book Title"/>
-                <button type="button" className="btn btn-primary">Search</button>
+                <input type="text" placeholder="Book Title" onChange={this.props.handleTitleChange} />
+                <button type="submit" className="btn btn-primary" onClick={this.props.searchBooks}>Search</button>
             </div>
         </form>
       </div>
